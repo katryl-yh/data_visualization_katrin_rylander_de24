2bf4915a36c958edc899b58c513ef9d87344d258
Katrin katryl-yh <katrin@rylander.eu>
2025-04-25 11:10:28 +0200

    final work on mplstyle

--- code_alongs/05_mplstyle/charts_utils.py
+++ code_alongs/05_mplstyle/charts_utils.py
@@ -1,3 +1,5 @@
+from matplotlib.ticker import FuncFormatter
+
 def horizontal_bar_options(ax, **options):
 
     ax.set_title(
@@ -20,3 +22,18 @@ def save_fig_from_ax(ax, save_path):
     fig = ax.get_figure()
     fig.tight_layout()
     fig.savefig(save_path)
+
+
+def thousands_formatter(ax, axis="x"):
+    formatter = FuncFormatter(
+        lambda val, pos: f"{int(val/1000)}K" if val else f"{val:.0f}"
+    )
+    if axis == "x":
+        ax.xaxis.set_major_formatter(formatter)
+    elif axis == "y":
+        ax.yaxis.set_major_formatter(formatter)
+    elif axis == "both":
+        ax.xaxis.set_major_formatter(formatter)
+        ax.yaxis.set_major_formatter(formatter)
+
+    return ax
