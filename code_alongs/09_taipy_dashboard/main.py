33775727c67b2e3d2380a155695905653a04cbd0
Katrin katryl-yh <katrin@rylander.eu>
2025-05-09 09:24:05 +0200

    changed main so dashboard reacts to default settings

--- code_alongs/09_taipy_dashboard/main.py
+++ code_alongs/09_taipy_dashboard/main.py
@@ -4,7 +4,6 @@ import pandas as pd
 from utils.constants import DATA_DIRECTORY
 from frontend.charts import create_municipality_bar
 
-
 df = pd.read_excel(
     DATA_DIRECTORY / "resultat-ansokningsomgang-2024.xlsx",
     sheet_name="Tabell 3",
@@ -32,19 +31,25 @@ def filter_data(state):
     )
 
 
+# def update_slider_max(state):
+#     df_municipality = filter_df_municipality(state.df, state.selected_educational_area)
+#     state.max_municipalities = len(df_municipality)
+#     print(state.max_municipalities)
+
+
 number_municipalities = 5
+max_municipalities = len(filter_df_municipality(df))
+
 selected_educational_area = "Data/IT"
 
-df_municipality = filter_df_municipality(df)
+df_municipality = filter_df_municipality(df, selected_educational_area).head(
+    number_municipalities
+)
+
 
 municipality_chart = create_municipality_bar(
     df_municipality, xlabel="# ANSÖKTA UTBILDNINGAR", ylabel="KOMMUN"
 )
-
-
-
-# --------------------------------------------------------------
-# --------------------------------------------------------------
 
 with tgb.Page() as page:
     with tgb.part(class_name="container card"):
@@ -62,8 +67,9 @@ with tgb.Page() as page:
                 tgb.slider(
                     "{number_municipalities}",
                     min=5,
-                    max=len(df_municipality),
+                    max="{max_municipalities}",
                     continuous=False,
+                    # on_change=filter_data
                 )
 
                 tgb.text("Välj utbildningsområde", mode="md")
@@ -71,6 +77,7 @@ with tgb.Page() as page:
                     "{selected_educational_area}",
                     lov=df["Utbildningsområde"].unique(),
                     dropdown=True,
+                    # on_change=update_slider_max,
                 )
 
                 tgb.button("FILTRERA DATA", class_name="plain", on_action=filter_data)
@@ -80,8 +87,5 @@ with tgb.Page() as page:
             tgb.table("{df}")
 
 
-# --------------------------------------------------------------
-# --------------------------------------------------------------
-
 if __name__ == "__main__":
-    Gui(page).run(dark_mode=False, use_reloader=False, port=8080)
+    Gui(page).run(dark_mode=False, use_reloader=True, port=8080)
